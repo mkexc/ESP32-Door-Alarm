de4c465369f46480a53face3f96bbc3fcf42cae4
Michelangelo Bartolomucci <3181748+mkexc@users.noreply.github.com>
2020-03-03 01:28:25 +0100

    Initial Release

---
+++ README.md
@@ -0,0 +1,62 @@
+# ESP32 DOOR ALARM
+
+This project has been made only from a **hobby** perspective.
+
+## Description
+This project aims to give you a modern and complete IoT door alarm to deploy on your esp32. 
+It has been built with esp32-arduino framework.
+
+It is based on the Magnetic Hall effect, which is why a magnet mounted on the door is required. 
+You can adapt it with a proxymity sensor if you want, but it will not be as accurate as this.  
+When the door opens, magnetic field decreases triggering the KY-024 hall sensor.   
+If the alarm is armed it sends an email to your email address configured in the code every 60 seconds, until the door closes or you unarm the alarm.
+When alarm is armed, the LED turns ON.
+It includes also an alarm manager accessible from [ESP32IP's]:80 (or with a DDNS) with a switch button.  
+Remember to configure in the code your SMTP,WiFi,EasyDDNS and Timezone settings.
+
+## Hardware
+- ESP32 DevKit
+- Generic LED(w/ 220 Ω resistor)
+- KY-024 Magnetic Hall sensor
+- Little Magnet
+
+## Dependencies
+
+- ESP32-Arduino framework
+- [NTPClient](https://github.com/taranais/NTPClient) Library (by taranais)
+- [ESP32 Mail Client](https://github.com/mobizt/ESP32-Mail-Client) Library (by mobizt)
+- [EasyDDNS](https://github.com/ayushsharma82/EasyDDNS) Library (by ayushsharma82)
+- [ESPAsyncWebServer](https://github.com/me-no-dev/ESPAsyncWebServer) Library (by me-no-dev)
+
+If you use PlatformIO you can install them directly from the integrated library manager, except for NTPClient which you should install manually because it's a fork.
+Same for Arduino IDE.
+
+## Compiling in Arduino IDE
+
+- Rename Alarm.cpp from *.cpp to *.ino
+- Remove function prototypes
+- Remove "#include <Arduino.h>"
+- Install deps
+- Insert personal Data(WiFi,SMTP etc...) into Code
+- Upload Firmware
+- Upload Data
+
+## Compiling in VSCode
+
+- Install PlatformIO
+- Configure platform.ini with your local ESP32's IP address (if you wan OTA Upload)
+- Install deps
+- Insert personal Data(WiFi,SMTP etc...) into Code
+- Upload Firmware
+- Upload File System Image
+
+## Circuit Example
+
+<img src="https://github.com/mkexc/ESP32_DoorAlarm/raw/master/common/images/Circuit.png" width="400">
+
+
+## Credits
+
+**Author**: Michelangelo Bartolomucci (**mkexc**)  
+If you liked this project and want to support further development, you can buy me a [🍻beer](https://www.paypal.me/mibart/5). :)  
+**License GNU AGPL**
---
+++ src/Alarm.cpp
@@ -0,0 +1,204 @@
+/*Credits
+
+Author: Michelangelo Bartolomucci (mkexc)  
+Follow me on github for other interesting projects! :)  
+License GNU AGPL */
+
+#include <Arduino.h>
+#include <Preferences.h>
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WiFiMulti.h>
+#include <WiFiUdp.h>
+#include <WiFiScan.h>
+#include <ETH.h>
+#include <WiFiClient.h>
+#include <WiFiSTA.h>
+#include <WiFiServer.h>
+#include <WiFiType.h>
+#include <WiFiGeneric.h>
+#include <NTPClient.h>
+#include <ESP32MailHTTPClient.h>
+#include <WiFiClientSecureESP32.h>
+#include <ESP32_MailClient.h>
+#include <RFC2047.h>
+#include <ESP32TimeHelper.h>
+#include <ssl_client32.h>
+#include <dummy.h>
+#include <EasyDDNS.h>
+#include <ESPmDNS.h>
+#include <ArduinoOTA.h>
+#include <SPIFFS.h>
+#include <ESPAsyncWebServer.h>
+
+#define LED 26 
+#define HALL 25
+
+//global variables
+char ssid[]= "............";
+char pass[]="............";
+char smtpServer[]="............";
+char smtpLogin[]="............";
+char smtpPass[]="............";
+char smtpRecipient[]="............";
+char smtpSenderEmail[]="............";
+char smtpSenderName[]="............";
+char smtpSubject[]="............";
+bool armed;
+int magn,timezone=+1; //set GMT timezone
+time_t currentTime,previousTime;
+
+//objects
+Preferences eeprom;
+SMTPData smtpData;
+WiFiUDP ntpUDP;
+NTPClient ntptime(ntpUDP,timezone*3600); 
+AsyncWebServer server(80);  //webserver, define port
+String LedState;
+
+//prototypes
+String processor(const String& var);
+
+//functions
+
+void setup() {
+  //get last setting before shutdown
+  eeprom.begin("allarme",false);
+  armed=eeprom.getInt("armed");
+  eeprom.end();
+  Serial.println(armed);
+
+  //set pins
+  pinMode(HALL,INPUT);
+  pinMode(LED,OUTPUT);
+  digitalWrite(LED,armed);
+  
+  Serial.begin(115200);
+
+  //WIFI Setup
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,pass);
+  WiFi.setHostname("ESP32");
+  
+  while(!WiFi.isConnected())
+  {
+    Serial.println(".");
+  }
+  Serial.println("Connected.");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+  
+  //SPIFFS Mount
+  if(!SPIFFS.begin(true))
+  {
+    Serial.println("Error mounting SPIFFS");
+    return;
+  }
+  //SMTP Server & Email Settings
+  smtpData.setLogin(smtpServer,587,smtpLogin,smtpPass);
+  smtpData.setPriority("High");
+  smtpData.setSubject(smtpSubject);
+  smtpData.addRecipient(smtpRecipient);
+  smtpData.setSender(smtpSenderName,smtpSenderEmail);
+
+  //NTP Time initial setup
+  ntptime.begin();  
+
+  //WebServer Settings and handling
+  if(MDNS.begin("esp32"))
+  {
+    Serial.println("MDNS Responder started");
+  }
+  server.on("/",HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", String(),false,processor);
+  });
+  server.on("/switch", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("ok");
+    armed = !armed;
+    digitalWrite(LED,armed);
+    eeprom.begin("allarme",false);
+    eeprom.putInt("armed",armed); //store in eeprom current value to resume from shutdown
+    eeprom.end();
+    delay(2000);
+    request->send(SPIFFS, "/index.html", String(),false,processor);
+  });
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+  server.begin();
+
+  //setup DDNS
+  EasyDDNS.service("............");
+  EasyDDNS.client("............","............","............");
+
+  previousTime=0;
+
+  //OTA Setup
+  ArduinoOTA.setPort(3232);
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  EasyDDNS.update(10000);
+  magn=digitalRead(HALL);
+  ArduinoOTA.handle();
+  currentTime=millis();
+  if(!(magn)&&(armed)&&(currentTime-previousTime>=60000))
+  {
+    previousTime=currentTime;
+    currentTime=millis();
+     ntptime.update();
+     String day = ntptime.getFormattedDate();
+     int T=day.indexOf("T");
+     smtpData.setMessage("Rilevata un' intrusione alle "+ntptime.getFormattedTime()+" il "+day.substring(0,T)+"",false);
+     MailClient.sendMail(smtpData);
+     Serial.println("Email sent!");
+  }
+  ArduinoOTA.handle();
+}
+
+//switch HTML variables
+String processor(const String& var)
+{
+  Serial.println(var);
+  if (var=="STATE")
+  {
+    if (armed)
+    {
+      LedState = "ON";
+    }
+    else
+    {
+      LedState = "OFF";
+    }
+    return LedState;
+  }
+  return String();
+}
